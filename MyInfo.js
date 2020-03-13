9d51ecc3127da275e74dbe71d2cbe2906f20ce21
prasadmunigolla <51947629+prasadmunigolla@users.noreply.github.com>
2020-03-13 16:23:27 +0530

    component life cycle

---
+++ MyInfo.js
@@ -0,0 +1,140 @@
+/*import React from 'react';
+import Footer from './Footer';
+function MyInfo(){
+    return(
+        <div>
+            <header>
+                <h1>this is header</h1>
+            </header>
+            <main>
+                <p><h6>this is main</h6></p>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+export default MyInfo*/
+
+
+
+/*import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+  
+class Test extends React.Component { 
+    constructor(props) 
+    { 
+        super(props); 
+        this.state = { hello : "World!" }; 
+    } 
+  
+    componentWillMount() 
+    { 
+        console.log("componentWillMount()"); 
+    } 
+  
+    componentDidMount() 
+    { 
+        console.log("componentDidMount()"); 
+    } 
+  
+    changeState() 
+    { 
+        this.setState({ hello : "Geek!" }); 
+    } 
+  
+    render() 
+    { 
+        return ( 
+            <div> 
+            <h1>GeeksForGeeks.org, Hello{ this.state.hello }</h1> 
+            <h2> 
+             <a onClick={this.changeState.bind(this)}>Press Here!</a> 
+            </h2> 
+            </div>); 
+    } 
+  
+    shouldComponentUpdate(nextProps, nextState) 
+    { 
+        console.log("shouldComponentUpdate()"); 
+        return true; 
+    } 
+  
+    componentWillUpdate() 
+    { 
+        console.log("componentWillUpdate()"); 
+    } 
+  
+    componentDidUpdate() 
+    { 
+        console.log("componentDidUpdate()"); 
+    } 
+} 
+  
+ReactDOM.render( 
+    <Test />, 
+    document.getElementById('root')); */
+
+
+    import React from 'react';
+
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 0
+      }
+      this.setNewNumber = this.setNewNumber.bind(this)
+   };
+   setNewNumber() {
+      this.setState({data: this.state.data + 1})
+   }
+   render() {
+      return (
+         <div>
+            <button onClick = {this.setNewNumber}>INCREMENT</button>
+            <Content myNumber = {this.state.data}></Content>
+         </div>
+      );
+   }
+}
+class Content extends React.Component {
+   componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+   shouldComponentUpdate(newProps, newState) {
+      return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
+   render() {
+      return (
+         <div>
+            <h3>{this.props.myNumber}</h3>
+         </div>
+      );
+   }
+}
+export default App;
+main.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+setTimeout(() => {
+   ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
